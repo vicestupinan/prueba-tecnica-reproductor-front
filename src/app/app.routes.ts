811709e8af610787b6eb9b38b73422505ed93d29
vicestupinan <88508615+vicestupinan@users.noreply.github.com>
2025-06-18 15:58:10 -0500

    feat: implementacion de rutas para el manejo de la app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PlaylistListComponent } from './components/playlist-list/playlist-list';
+import { PlaylistFormComponent } from './components/playlist-form/playlist-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PlaylistListComponent },
+  { path: 'crear', component: PlaylistFormComponent },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PlaylistListComponent } from './components/playlist-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PlaylistListComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'prueba-tecnica-reproductor-front';
+  protected title = 'prueba-tecnica-playlist-front';
 }
